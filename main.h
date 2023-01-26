7f214f213f9ba02aee004c3abf6efe48e148b1e1
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-01-27 00:32:19 +0300

    updated main.h

--- main.h
+++ main.h
@@ -20,16 +20,15 @@
 
 /**
  * struct fmt - Struct op
- *
  * @fmt: The format.
  * @fn: The function associated.
  */
+
 struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
 };
-
 
 /**
  * typedef struct fmt fmt_t - Struct op
@@ -37,6 +36,7 @@ struct fmt
  * @fmt: The format.
  * @fm_t: The function associated.
  */
+
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
@@ -45,7 +45,7 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
-/* Funtions to print chars and strings */
+/**** Funtions to print chars and strings ****/
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -70,11 +70,11 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function to print non printable characters */
+/*** Function to print non printable characters ***/
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/***** Funcion to print memory address *****/
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
